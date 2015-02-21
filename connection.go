d5c5144cd4f8f1582e9d580d170900914a2645e6
Alfonso <alfonso.fernandez@rocket-internet.de>
2015-02-21 20:01:11 +0100

    make server variable

--- connection.go
+++ connection.go
@@ -7,8 +7,8 @@ import (
 var Z *ara.Session
 
 func init() {
-	s, err := ara.Connect("http://localhost:8529", "", "", false)
-	//	s, err := ara.Connect(ARANGO_SERVER, ARANGO_USER, ARANGO_PASS, false)
+	//s, err := ara.Connect("http://localhost:8529", "", "", false)
+	s, err := ara.Connect(ARANGO_SERVER, ARANGO_USER, ARANGO_PASS, false)
 	if err != nil {
 		panic("Can't connect to ")
 	}
